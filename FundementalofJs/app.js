a1a4bd20ef5e091b84e93189ebd2dfa559af004f
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-19 18:16:33 +0530

    variables

---
+++ FundementalofJs/app.js
@@ -0,0 +1,68 @@
+//Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting ='hello';
+console.log(greeting);
+console.log([1,2,3,4,5,6]);
+console.log({a:1,b:2});
+//alert("Hello World");
+console.table({a:1,b:2});
+console.error('this is some error');
+document.querySelector('h1').style.color='red'
+console.clear();
+console.warn('This is warning');
+console.time('Hello');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.timeEnd('Hello');
+
+//var,let and const
+// var name='Siddharth Singh';
+// console.log(name);
+// name='Singh';
+// console.log(name);
+// //INIT VAR
+// var greeting;
+// console.log(greeting);
+// greeting='Hello';
+// console.log(greeting);
+
+//letter,number,_,$ 
+//can not start with number
+
+// Multi wold vars
+
+ var firstName='Siddharth';//camel case
+ var first_name='Singh';//underscore 
+ var FirstName='Mohan';//Pascel case
+ var firstname
+
+
+ //Let
+//  let name;
+// name='Siddharth Singh';
+// console.log(name);
+// name='Singh';
+// console.log(name);
+
+//Const
+
+// const name='Siddharth Singh';
+// console.log(name);
+// console.log(name);
+
+const persion={
+    name:'Singh',
+    age:30
+}
+persion.name='Sara';
+persion.age=30
+console.log(persion);
+
+const number=[1,2,3,4,6];
+number.push(7)
+console.log(number);
+
